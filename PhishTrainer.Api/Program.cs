80d066c33890ca2c1f5b5aa0ab3290b6501effa8
Matthew Palmer <81492324+zy538324@users.noreply.github.com>
2026-01-27 08:30:53 +0000

    Update Program.cs

--- PhishTrainer.Api/Program.cs
+++ PhishTrainer.Api/Program.cs
@@ -1,3 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using PhishTrainer.Api.Data;
+using PhishTrainer.Api.Middleware;
+using PhishTrainer.Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PhishDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ITenantResolver, TenantResolver>();
+builder.Services.AddScoped<IMailService, MailService>();
+builder.Services.AddScoped<ICampaignService, CampaignService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware<TenantMiddleware>();
+app.UseCors();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -39,3 +83,4 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
