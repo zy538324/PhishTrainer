184961c8cfc991ea94ed2d150612ac410ff4f34a
Matthew Palmer <81492324+zy538324@users.noreply.github.com>
2026-01-27 08:24:54 +0000

    Add TargetGroup class and update TargetUser
    Added TargetGroup class and restructured TargetUser class.

--- PhishTrainer.Api/Models/TargetUser.cs
+++ PhishTrainer.Api/Models/TargetUser.cs
@@ -1,3 +1,16 @@
+namespace PhishTrainer.Api.Models;
+
+public class TargetGroup : IMustHaveTenant
+{
+    public int Id { get; set; }
+    public int TenantId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public List<TargetUser> Targets { get; set; } = new();
+    
+    public Tenant Tenant { get; set; } = null!;
+}
+
 public class TargetUser : IMustHaveTenant
 {
     public int Id { get; set; }
